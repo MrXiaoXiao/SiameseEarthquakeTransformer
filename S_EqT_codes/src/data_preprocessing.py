9c1d4a114ec0312bfe3ef383af6e2b30bf61064d
MrXiaoXiao <779182110@qq.com>
2021-04-26 14:45:28 +0800

    Update_T01_02

--- S_EqT_codes/src/data_preprocessing.py
+++ S_EqT_codes/src/data_preprocessing.py
@@ -11,15 +11,17 @@ def normalize_by_std(data_in):
 
     return data_in
 
-def get_ori_response_list(cfgs, spt_t_eqt, sst_t_eqt, encoded_t, encoded_s):
+def get_response_list_for_vis(cfgs, spt_t_eqt, sst_t_eqt, encoded_t, encoded_s):
 
     RSRN_lengths = cfgs['Model']['RSRN_Encoded_lengths']
     RSRN_channels = cfgs['Model']['RSRN_Encoded_channels']
-    encoder_encoded_list = cfgs['Model']['Encoder_concate_list']
-    encoder_encoded_lengths = cfgs['Model']['Encoder_concate_lengths']
-    encoder_encoded_channels = cfgs['Model']['Encoder_concate_channels']
+    #encoder_encoded_list = cfgs['Model']['Encoder_concate_list']
+    #encoder_encoded_lengths = cfgs['Model']['Encoder_concate_lengths']
+    #encoder_encoded_channels = cfgs['Model']['Encoder_concate_channels']
 
-    ori_response_list = list()
+    ori_response_list_for_vis = list()
+    enhanced_response_list_for_vis = list()
+
     t_spt_t = float(spt_t_eqt/6000.0)
     t_sst_t = float(sst_t_eqt/6000.0)
 
@@ -32,10 +34,36 @@ def get_ori_response_list(cfgs, spt_t_eqt, sst_t_eqt, encoded_t, encoded_s):
         encoded_t[rdx] = encoded_t[rdx][:,template_s:template_e,:]/float(template_w)
         encoded_t[rdx] = encoded_t[rdx].reshape([1,template_w,1,int(RSRN_channels[rdx])])
         encoded_s[rdx] = encoded_s[rdx].reshape([1,int(RSRN_lengths[rdx]),1,int(RSRN_channels[rdx])])
-        ori_response_list.append(np.copy(encoded_t[rdx]))
-        ori_response_list.append(np.copy(encoded_s[rdx]))
+        ori_response_list_for_vis.append(np.copy(encoded_t[rdx]))
+        ori_response_list_for_vis.append(np.copy(encoded_s[rdx]))
         
-    return ori_response_list
+        # channel-wise normalization
+        for channel_dx in range(int(RSRN_channels[rdx])):
+            encoded_s[rdx][0,:,0,channel_dx] -= np.max(encoded_s[rdx][0,:,0,channel_dx])
+            half_window_len = int( 200.0*temp_length/6000.0   ) + 1
+            
+            encoded_s[rdx][0,:half_window_len,0,channel_dx] = encoded_s[rdx][0,half_window_len,0,channel_dx]
+            encoded_s[rdx][0,-half_window_len:,0,channel_dx] =  encoded_s[rdx][0,-half_window_len,0,channel_dx]
+
+            encoded_s[rdx][0,:,0,channel_dx] *= -1.0
+            encoded_s[rdx][0,:,0,channel_dx] -= np.mean(encoded_s[rdx][0,:,0,channel_dx])
+            t_max = np.max(np.abs(encoded_s[rdx][0,:,0,channel_dx]))
+            if t_max < 0.001:
+                t_max = 1
+            encoded_s[rdx][0,:,0,channel_dx] /= t_max
+
+            encoded_t[rdx][0,:,0,channel_dx] -= np.max(encoded_t[rdx][0,:,0,channel_dx])
+            encoded_t[rdx][0,:,0,channel_dx] *= -1.0
+            encoded_t[rdx][0,:,0,channel_dx] -= np.mean(encoded_t[rdx][0,:,0,channel_dx])
+            t_max = np.max(np.abs(encoded_t[rdx][0,:,0,channel_dx]))
+            if t_max < 0.001:
+                t_max = 1
+            encoded_t[rdx][0,:,0,channel_dx] /= t_max
+            encoded_t[rdx][0,:,0,channel_dx] /= float(template_w)
+        enhanced_response_list_for_vis.append(encoded_t[rdx])
+        enhanced_response_list_for_vis.append(encoded_s[rdx])
+
+    return ori_response_list_for_vis, enhanced_response_list_for_vis
 
 def get_siamese_input_list(cfgs, spt_t_eqt, sst_t_eqt, encoded_t, encoded_s):
 
--- S_EqT_codes/src/misc.py
+++ S_EqT_codes/src/misc.py
@@ -4,6 +4,237 @@ import h5py
 import matplotlib.pyplot as plt
 from random import shuffle
 
+def plot_P_branch_responses(cfgs):
+    plot_t = np.arange(0,30.01,0.01)
+    plt.figure(figsize=(16,12))
+    ax1 = plt.subplot2grid((6,6),(0,4),colspan=2,rowspan=2)
+    plt.title('Simplified EqT diagram',fontsize=14)n
+    img = mpimg.imread('EqT_Fig5_use.jpg')
+    plt.imshow(img,aspect='auto')
+    plt.xticks([])
+    plt.yticks([])
+    ax1.axis('off')
+
+    ax_t1 = plt.subplot2grid((6,6),(0,0),rowspan=2,colspan=2)
+    for idx in range(3):
+        plt.plot(plot_t,data_t[0:3001,idx]/np.max(np.abs(data_t[0:3001,idx]))+idx*2 + 2,color='k')
+        if idx == '0':
+            plt.text(28, idx*2 + 2 + 0.1, 'E')
+        if idx == '1':
+            plt.text(28, idx*2 + 2 + 0.1, 'N')
+        if idx == '2':
+            plt.text(28, idx*2 + 2 + 0.1, 'Z')
+
+    plt.plot([spt_t_eqt/100.0,spt_t_eqt/100.0],[1,8],color='b',label='EqT P')
+    plt.plot([sst_t_eqt/100.0,sst_t_eqt/100.0],[1,8],color='r',label='EqT S')
+    plt.title('Template seismogram from station CI.DEC\nStart Time: 2020-08-08T16:46:00',fontsize=14)
+    plt.ylim([1,7.5])
+    plt.legend(loc='upper right',prop= {'size':14})
+    plt.yticks([])
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.xlim([0,30])
+    plt.xlabel('time (s)',fontsize=14)
+
+    ax_t2 = plt.subplot2grid((6,6),(2,0),colspan=2)
+    rdx = 29
+    t_len = int(len(encoded_t_plot[rdx][0,:,0])/2.0)
+    for channel_dx in range(int(RSRN_channels[rdx])):
+        plt.plot(encoded_t_plot[rdx][0,:t_len,channel_dx] + 0 * 2,color='k')
+    plt.xlim([0,t_len])
+    plt.title('OTR conv1d_35',fontsize=14)
+    plt.plot([spt_t_eqt/4.0,spt_t_eqt/4.0],[-50,50],color='b',label='EqT P')
+    plt.plot([sst_t_eqt/4.0,sst_t_eqt/4.0],[-50,50],color='r',label='EqT S')
+    t_min_plot = np.min(encoded_t_plot[rdx])
+    t_max_plot = np.max(encoded_t_plot[rdx])
+    t_gain = (t_max_plot - t_min_plot)*0.15
+    plt.ylim([t_min_plot - t_gain,t_max_plot + t_gain])
+    t_ax=plt.gca();t_ax.spines['right'].set_color('b');t_ax.spines['top'].set_color('b');t_ax.spines['bottom'].set_color('b');t_ax.spines['left'].set_color('b');t_ax.spines['right'].set_linewidth(3);t_ax.spines['top'].set_linewidth(3);t_ax.spines['bottom'].set_linewidth(3);t_ax.spines['left'].set_linewidth(3)
+    plt.text(-100,t_min_plot+t_gain*0.15,'Amplitude',fontsize=14,rotation=90)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    ax_t3 = plt.subplot2grid((6,6),(3,0),colspan=2)
+    rdx = 29
+    t_len = int(len(encoded_t_plot[rdx][0,:,0])/2.0)
+    start_dx = int(spt_t_eqt*int(RSRN_lengths[rdx])/6000.0)
+    end_dx = start_dx + len(encoded_t[rdx][0,:,0,0])
+    temp_x_plot = np.arange(start_dx,end_dx,1)
+    for channel_dx in range(int(RSRN_channels[rdx])):
+        plt.plot(temp_x_plot, encoded_t[rdx][0,:,0,channel_dx] + 0 * 2,color='k')
+    plt.xlim([0,t_len])
+    plt.title('ETR conv1d_35',fontsize=14)
+    plt.plot([spt_t_eqt/4.0,spt_t_eqt/4.0],[-50,50],color='b',label='EqT P')
+    plt.plot([sst_t_eqt/4.0,sst_t_eqt/4.0],[-50,50],color='r',label='EqT S')
+    t_min_plot = np.min(encoded_t[rdx])
+    t_max_plot = np.max(encoded_t[rdx])
+    t_gain = (t_max_plot - t_min_plot)*0.15
+    plt.ylim([t_min_plot - t_gain,t_max_plot + t_gain])
+    t_ax=plt.gca();t_ax.spines['right'].set_color('b');t_ax.spines['top'].set_color('b');t_ax.spines['bottom'].set_color('b');t_ax.spines['left'].set_color('b');t_ax.spines['right'].set_linewidth(3);t_ax.spines['top'].set_linewidth(3);t_ax.spines['bottom'].set_linewidth(3);t_ax.spines['left'].set_linewidth(3)
+    plt.text(-140,t_min_plot+t_gain*0.15,'Amplitude',fontsize=14,rotation=90)
+    plt.xlabel('Array Index (N)',fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    ax_t4 = plt.subplot2grid((6,6),(2,4),colspan=2)
+    rdx = 29
+    t_len = int(len(encoded_t_plot[rdx][0,:,0])/2.0)
+    for channel_dx in range(int(RSRN_channels[rdx])):
+        plt.plot(no_normal_corr[rdx][0,:t_len,0,channel_dx]/57.0 + 0 * 2,color='k')
+    plt.xlim([0,t_len])
+    plt.title('OCC conv1d_35',fontsize=14)
+    t_min_plot = np.min(no_normal_corr[rdx])/57.0
+    t_max_plot = np.max(no_normal_corr[rdx])/57.0
+    t_gain = (t_max_plot - t_min_plot)*0.15
+    plt.ylim([t_min_plot - t_gain,t_max_plot + t_gain])
+    plt.plot([SEqT_P/4.0,SEqT_P/4.0],[-50,50],color='b',linestyle='--',label='S-EqT P')
+    #plt.plot([SEqT_S/4.0,SEqT_S/4.0],[-50,50],color='r',linestyle='--',label='S-EqT S')
+    t_ax=plt.gca();t_ax.spines['right'].set_color('b');t_ax.spines['top'].set_color('b');t_ax.spines['bottom'].set_color('b');t_ax.spines['left'].set_color('b');t_ax.spines['right'].set_linewidth(3);t_ax.spines['top'].set_linewidth(3);t_ax.spines['bottom'].set_linewidth(3);t_ax.spines['left'].set_linewidth(3)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    ax_s1 = plt.subplot2grid((6,6),(0,2),rowspan=2,colspan=2)
+    for idx in range(3):
+        plt.plot(plot_t,data_s[0:3001,idx]/np.max(np.abs(data_s[0:3001,idx]))+idx*2 + 2,color='k')
+        if idx == '0':
+            plt.text(28, idx*2 + 2 + 0.1, 'E')
+        if idx == '1':
+            plt.text(28, idx*2 + 2 + 0.1, 'N')
+        if idx == '2':
+            plt.text(28, idx*2 + 2 + 0.1, 'Z')
+
+    plt.yticks([])
+    plt.plot([SEqT_P/100.0,SEqT_P/100.0],[1,8],color='b',linestyle='--',label='S-EqT P')
+    #plt.plot([SEqT_S/100.0,SEqT_S/100.0],[1,8],color='r',linestyle='--',label='S-EqT S')
+    plt.xlim([0,30])
+    plt.ylim([1,7.5])
+    plt.legend(loc='upper right',prop= {'size':14})
+    #plt.text(0,7.5,'(c)',fontsize=20)
+    plt.title('Searching seismogram from station CI.RIN\nStart Time: 2020-08-08T16:46:00',fontsize=14)
+    plt.xlabel('time (s)',fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    ax_s2 = plt.subplot2grid((6,6),(2,2),colspan=2)
+    rdx = 29
+    t_len = int(len(siamese_input_list_no_normal[rdx*2+1][0,:,0,0])/2.0)
+    for channel_dx in range(int(RSRN_channels[rdx])):
+        plt.plot(siamese_input_list_no_normal[rdx*2+1][0,:t_len,0,channel_dx] + 0 * 2,color='k')    
+    plt.xlim([0,t_len])
+    plt.title('OSR conv1d_35',fontsize=14)
+    t_min_plot = np.min(siamese_input_list_no_normal[rdx*2+1])
+    t_max_plot = np.max(siamese_input_list_no_normal[rdx*2+1])
+    t_gain = (t_max_plot - t_min_plot)*0.15
+    plt.ylim([t_min_plot - t_gain,t_max_plot + t_gain])
+    plt.plot([SEqT_P/4.0,SEqT_P/4.0],[-50,50],color='b',linestyle='--',label='S-EqT P')
+    #plt.plot([SEqT_S/4.0,SEqT_S/4.0],[-50,50],color='r',linestyle='--',label='S-EqT S')
+    t_ax=plt.gca();t_ax.spines['right'].set_color('b');t_ax.spines['top'].set_color('b');t_ax.spines['bottom'].set_color('b');t_ax.spines['left'].set_color('b');t_ax.spines['right'].set_linewidth(3);t_ax.spines['top'].set_linewidth(3);t_ax.spines['bottom'].set_linewidth(3);t_ax.spines['left'].set_linewidth(3)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    ax_s3 = plt.subplot2grid((6,6),(3,2),colspan=2)
+    rdx = 29
+    t_len = int(len(encoded_s[rdx][0,:,0,0])/2.0)
+    for channel_dx in range(int(RSRN_channels[rdx])):
+        plt.plot(encoded_s[rdx][0,:t_len,0,channel_dx] + 0 * 2,color='k')
+    plt.xlim([0,t_len])
+    plt.title('ESR conv1d_35',fontsize=14)
+    t_min_plot = np.min(encoded_s[rdx])
+    t_max_plot = np.max(encoded_s[rdx])
+    t_gain = (t_max_plot - t_min_plot)*0.15
+    plt.ylim([t_min_plot - t_gain,t_max_plot + t_gain])
+    plt.plot([SEqT_P/4.0,SEqT_P/4.0],[-50,50],color='b',linestyle='--',label='S-EqT P')
+    #plt.plot([SEqT_S/4.0,SEqT_S/4.0],[-50,50],color='r',linestyle='--',label='S-EqT S')
+    t_ax=plt.gca();t_ax.spines['right'].set_color('b');t_ax.spines['top'].set_color('b');t_ax.spines['bottom'].set_color('b');t_ax.spines['left'].set_color('b');t_ax.spines['right'].set_linewidth(3);t_ax.spines['top'].set_linewidth(3);t_ax.spines['bottom'].set_linewidth(3);t_ax.spines['left'].set_linewidth(3)
+    plt.xlabel('Array Index (N)',fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    ax_s4 = plt.subplot2grid((6,6),(3,4),colspan=2)
+    rdx = 29
+    t_len = int(len(encoded_s[rdx][0,:,0,0])/2.0)
+    for channel_dx in range(int(RSRN_channels[rdx])):
+        plt.plot(corr_res[rdx][0,:,0,channel_dx] + 0 * 2,color='k')
+    plt.xlim([0,t_len])
+    plt.title('ECC conv1d_35',fontsize=14)
+    t_min_plot = np.min(corr_res[rdx])
+    t_max_plot = np.max(corr_res[rdx])
+    t_gain = (t_max_plot - t_min_plot)*0.15
+    plt.ylim([t_min_plot - t_gain,t_max_plot + t_gain])
+    plt.plot([SEqT_P/4.0,SEqT_P/4.0],[-50,50],color='b',linestyle='--',label='S-EqT P')
+    #plt.plot([SEqT_S/4.0,SEqT_S/4.0],[-50,50],color='r',linestyle='--',label='S-EqT S')
+    t_ax=plt.gca();t_ax.spines['right'].set_color('b');t_ax.spines['top'].set_color('b');t_ax.spines['bottom'].set_color('b');t_ax.spines['left'].set_color('b');t_ax.spines['right'].set_linewidth(3);t_ax.spines['top'].set_linewidth(3);t_ax.spines['bottom'].set_linewidth(3);t_ax.spines['left'].set_linewidth(3)
+    plt.xlabel('Array Index (N)',fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    ax_eqt_final_resp = plt.subplot2grid((6,6),(4,0),colspan=3)
+    rdx = 31
+    t_len = int(len(siamese_input_list_no_normal[rdx*2+1][0,:,0,0])/2.0)
+    for channel_dx in range(int(RSRN_channels[rdx])):
+        plt.plot(siamese_input_list_no_normal[rdx*2+1][0,:t_len,0,channel_dx] + 0 * 2,color='k')    
+    plt.xlim([0,t_len])
+    plt.title('OSR conv1d_35',fontsize=14)
+    t_min_plot = np.min(siamese_input_list_no_normal[rdx*2+1])
+    t_max_plot = np.max(siamese_input_list_no_normal[rdx*2+1])
+    t_gain = (t_max_plot - t_min_plot)*0.15
+    plt.ylim([t_min_plot - t_gain,t_max_plot + t_gain])
+    plt.plot([SEqT_P,SEqT_P],[-50,50],color='b',linestyle='--',label='S-EqT P')
+    #plt.plot([SEqT_S,SEqT_S],[-50,50],color='r',linestyle='--',label='S-EqT S')
+    #t_ax=plt.gca();t_ax.spines['right'].set_color('b');t_ax.spines['top'].set_color('b');t_ax.spines['bottom'].set_color('b');t_ax.spines['left'].set_color('b');t_ax.spines['right'].set_linewidth(3);t_ax.spines['top'].set_linewidth(3);t_ax.spines['bottom'].set_linewidth(3);t_ax.spines['left'].set_linewidth(3)
+    plt.xlabel('Array Index (N)',fontsize=14)
+    plt.title('Response of the penultimate layer in P branch of EqT model',fontsize=14)
+    plt.ylabel('Amplitude',fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    ax_seqt_final_resp = plt.subplot2grid((6,6),(4,3),colspan=3)
+    for idx in range(7):
+        plt.plot(SEqT_final_response[0,:,0,idx],color='k')
+    t_min_plot = np.min(SEqT_final_response)
+    t_max_plot = np.max(SEqT_final_response)
+    t_gain = (t_max_plot - t_min_plot)*0.15
+    plt.ylim([-20,t_max_plot + t_gain])
+    plt.plot([SEqT_P,SEqT_P],[-40,40],color='b',linestyle='--',label='S-EqT P')
+    #plt.plot([SEqT_S,SEqT_S],[-40,40],color='r',linestyle='--',label='S-EqT S')
+    plt.xlim([0,t_len])
+    #plt.ylabel('Amplitude',fontsize=14)
+    plt.xlabel('Array Index (N)',fontsize=14)
+    plt.title('Response of the penultimate layer in P branch of S-EqT model',fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    ax_eqt_final = plt.subplot2grid((6,6),(5,0),colspan=3)
+    plt.plot(plot_t,res_search[2][0,0:3001,0],color='k')
+    plt.ylim([-0.05,0.3])
+    plt.xlim([0,30])
+    plt.plot([SEqT_P/100.0,SEqT_P/100.0],[-1,1],color='b',linestyle='--',label='S-EqT P')
+    #plt.plot([SEqT_S/100.0,SEqT_S/100.0],[-1,1],color='r',linestyle='--',label='S-EqT S')
+    plt.xlabel('time (s)',fontsize=14)
+    plt.ylabel('Probability',fontsize=14)
+    plt.title('P phase probabilities by EqT model',fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+
+    ax_seqt_final = plt.subplot2grid((6,6),(5,3),colspan=3)
+    plt.plot(plot_t,pred_res[-1][0,0:3001,0,0],color='k')
+    plt.xlim([0,30])
+    plt.ylim([-0.05,0.3])
+    plt.plot([SEqT_P/100.0,SEqT_P/100.0],[-1,1],color='b',linestyle='--',label='S-EqT P')
+    #plt.plot([SEqT_S/100.0,SEqT_S/100.0],[-1,1],color='r',linestyle='--',label='S-EqT S')
+    #plt.xlabel('time (s)',fontsize=14)
+    plt.title('P phase probabilities by S-EqT model',fontsize=14)
+    plt.xlabel('time (s)',fontsize=14)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+    return
+
+
+
 def sort_STEAD_csv_by_earthquake(csv_file):
     """
     read STEAD csv file and save a new file sorted by earthquakes
